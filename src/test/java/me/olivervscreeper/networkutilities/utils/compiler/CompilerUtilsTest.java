32338ed4d71a7c79cda52783015a3907344399c6
oliverdunk <dev@olivervscreeper.co.uk>
2015-10-28 21:44:51 +0000

    Don't rely on HasteBin cache for CompilerUtilsTest

--- src/test/java/me/olivervscreeper/networkutilities/utils/compiler/CompilerUtilsTest.java
+++ src/test/java/me/olivervscreeper/networkutilities/utils/compiler/CompilerUtilsTest.java
@@ -3,6 +3,8 @@ package me.olivervscreeper.networkutilities.utils.compiler;
 import junit.framework.TestCase;
 import me.olivervscreeper.networkutilities.utils.PasteUtils;
 
+import java.io.*;
+
 /**
  * @author OliverVsCreeper
  */
@@ -11,9 +13,26 @@ public class CompilerUtilsTest extends TestCase {
     public static boolean hasRun = false;
 
     public void testGenerateClass() throws Exception {
-        CompilerClass test = (CompilerClass) CompilerUtils.generateClass(PasteUtils.getPaste("iwapibipin")).newInstance();
+        System.out.println("* CompilerUtilsTest: testGenerateClass()");
+        CompilerClass test = (CompilerClass) CompilerUtils.generateClass(loadFile()).newInstance();
         test.run();
         assertTrue(hasRun);
+    }
+
+    public String loadFile(){
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        File file = new File("src/test/java/me/olivervscreeper/networkutilities/utils/compiler/CompilerUtilsTestRunnable.txt");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String code = "";
+            while(reader.ready()){
+                code = code + reader.readLine();
+            }
+            return code;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 
 }
